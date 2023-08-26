f8b122c3bad124b5af9d14c34206723a7ea3499e
fastmustach3 <115436920+fastmustach3@users.noreply.github.com>
2023-08-25 21:11:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+onst serviceList = document.getElementById('service-list');
+
+// An array of services
+const services = ['Web Development', 'Graphic Design', 'Digital Marketing'];
+
+// Populate the service list
+services.forEach(service => {
+  const listItem = document.createElement('li');
+  listItem.textContent = service;
+  serviceList.appendChild(listItem);
+});
